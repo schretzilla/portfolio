41e904d61203df2be642e1f40e68d1a33ca8f2d5
schretzilla <schretzilla@gmail.com>
2016-06-30 18:16:12 -0400

    added self help page

--- mysite/portfolio/urls.py
+++ mysite/portfolio/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^home/$', views.home, name='home'),
+    url(r'^self-help/$', views.self_help, name='self-help'),
+
 ]
--- mysite/portfolio/views.py
+++ mysite/portfolio/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'portfolio/home.html', context=None)
+
+def self_help(request):
+    return render(request, 'portfolio/self-help.html', context=None)
