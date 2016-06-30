9b71eb391b8469ff30dc6a636b7052462f94b463
schretzilla <schretzilla@gmail.com>
2016-06-30 16:23:28 -0400

    original commit

---
+++ djangoenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/schretzilla/Desktop/Development/python/Django/schretzilla.com/django-env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
